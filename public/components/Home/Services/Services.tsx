db18b29c2107d5025c3438e317f4d25a25c47a48
Ama-Dombawela <amadombawela@gmail.com>
2026-03-29 00:33:49 +0530

    Added Services folder structure

---
+++ public/components/Home/Services/Services.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div className='pt-16 pb-16'>
+        <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
+            Collaborate with me <br/> to create  smart solutions that bring your <br/> ideas to life
+        </h1>
+
+    </div>
+  )
+}
+
+export default services
