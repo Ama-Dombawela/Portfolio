979cbc391a4b77dcf9a81b5b1d8b36e087999d30
Ama-Dombawela <amadombawela@gmail.com>
2026-05-03 23:02:23 +0530

    Changed the image and updated the home section

--- public/components/Home/Home.tsx
+++ public/components/Home/Home.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import Services from './Services/Services'
+import Projects from './Projects/Projects'
+import Skills from './Skills/Skills'
+import Education from './Education/Education'
+import Contact from './Contact/Contact'
+import Resume from './Resume/Resume'
+import Footer from './Footer/Footer'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero/>
       <Services/>
-
+      <Projects/>
+      <Skills/>
+      <Education/>
+      <Resume/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
